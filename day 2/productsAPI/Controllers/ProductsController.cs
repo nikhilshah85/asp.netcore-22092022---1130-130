cdcd9109af8d9f4f6cf33c20841d8ee5e41f2a7f
Nikhil <nikhil.shah2008@gmail.com>
2022-09-23 13:19:36 +0530

    Day 2 WebAPI

---
+++ day 2/productsAPI/Controllers/ProductsController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using productsAPI.Model;
+namespace productsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //we create crud actions here
+        //they need to be based on HTTP Standards
+        //Method should have return type as IActionResult
+        //it should return Http Statud Code
+
+        Product pObj = new Product(); //this is a crime 
+                                      // we should not be creating any object as we are not habbitual to
+                                      //destrory the object
+                                      //USE DI instead
+
+        #region HTTPGET
+
+        [HttpGet]
+        [Route("plist")]
+        public IActionResult GetAllProducts()
+        {
+            return Ok(pObj.GetALlProducts());
+        }
+
+        [HttpGet]
+        [Route("plist/id")]
+        public IActionResult GetProductById(int id)
+        {
+
+            try
+            {
+                return Ok(pObj.GetProductById(id));
+            }
+            catch(Exception es)
+            {
+                return NotFound(es.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("plist/cat")]
+        public IActionResult GetProdutByCategory(string cat)
+        {
+            return Ok(pObj.GetProductByCategory(cat));
+        }
+
+        [HttpGet]
+        [Route("plist/instock")]
+        public IActionResult GetProductByAvability(bool instock)
+        {
+            return Ok(pObj.GetProductByAvability(instock));
+        }
+        #endregion
+
+        [HttpPost]
+        [Route("plist/add")]
+        public IActionResult AddNewProduct(Product newP)
+        {
+            return Created("",pObj.AddNewProduct(newP));
+        }
+
+        [HttpDelete]
+        [Route("plist/delete")]
+        public IActionResult DeleteProduct(int pid)
+        {
+            return Accepted(pObj.DeleteProduct(pid));
+        }
+        [HttpPut]
+        [Route("plist/edit")]
+        public IActionResult UpdateProduct(Product p)
+        {
+            return Accepted(pObj.EditProduct(p));
+        }
+
+    }
+}
---
+++ day 2/productsAPI/Model/Products.cs
@@ -0,0 +1,101 @@
+﻿namespace productsAPI.Model
+{
+    public class Product
+    {
+        #region Properties
+        public int pId { get; set; }
+        public string pName { get; set; }
+        public string pCategory { get; set; }
+        public double pPrice { get; set; }
+        public bool pIsInstock { get; set; }
+        #endregion
+
+        #region Get Methods
+        static List<Product> pList = new List<Product>()
+        {
+            new Product(){ pId=101, pName="Pepsi", pCategory="Cold-Drink", pIsInstock=true, pPrice=50},
+            new Product(){ pId=102, pName="Iphone", pCategory="Electronics", pIsInstock=false, pPrice=150000},
+            new Product(){ pId=103, pName="Air-Pods", pCategory="Electronics", pIsInstock=true, pPrice=27000},
+            new Product(){ pId=104, pName="IWatch", pCategory="Electronics", pIsInstock=true, pPrice=49000},
+            new Product(){ pId=105, pName="Fanta", pCategory="Cold-Drink", pIsInstock=false, pPrice=75}
+        };
+
+
+        public List<Product> GetALlProducts()
+        {
+            return pList;
+        }
+
+        public List<Product> GetProductByCategory(string category)
+        {
+            var p = pList.FindAll(pr => pr.pCategory == category);
+            return p;
+        }
+
+        public Product GetProductById(int id)
+        {
+            var p = pList.Find(pr => pr.pId == id);
+            if (p != null)
+            {
+                return p;
+            }
+            throw new Exception("Product Not Found");
+        }
+
+        public List<Product> GetProductByAvability(bool para)
+        {
+            var p = pList.FindAll(pr => pr.pIsInstock == para);
+            return p;
+        }
+        #endregion
+
+        #region Add, Delete and Update
+        public string AddNewProduct(Product newProduct)
+        {//do data validation
+            pList.Add(newProduct);
+            return "Product Added Successfully";
+        }
+
+        public string DeleteProduct(int pid)
+        {
+            var p = pList.Find(pr => pr.pId == pid);
+            if (p!=null)
+            {
+                pList.Remove(p);
+                return "Product Removed Successfully";
+            }
+            throw new Exception("Product Not Found");
+        }
+
+        public string EditProduct(Product changes)
+        {
+            var p = pList.Find(pr => pr.pId == changes.pId);
+            if (p != null)
+            {
+                p.pName = changes.pName;
+                p.pCategory = changes.pCategory;
+                p.pPrice = changes.pPrice;
+                p.pIsInstock = changes.pIsInstock;
+                return "Product Updated";
+            }
+            throw new Exception("Product Not Found");
+        }
+        #endregion
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
