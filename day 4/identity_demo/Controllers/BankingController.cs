93818b9e824cea4f1a79f64486551f4334577bf1
Nikhil <nikhil.shah2008@gmail.com>
2022-09-27 13:01:06 +0530

    Identity

---
+++ day 4/identity_demo/Controllers/BankingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace identity_demo.Controllers
+{
+
+    [Authorize]
+    public class BankingController : Controller
+    {
+        [AllowAnonymous] //any1 can access this
+        public IActionResult Bankinghome()
+        {
+            return View();
+        }
+        [AllowAnonymous] //any1 can access this
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult Transfer()
+        {
+            return View();
+        }
+        public IActionResult downloadstatement()
+        {
+            return View();
+        }
+    }
+}
---
+++ day 4/identity_demo/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace identity_demo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
